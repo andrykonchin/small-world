6935a02dbacdeddd1cf40917d078db1298a81875
Maxim Romashchenko <max@anahoret.com>
2012-05-01 13:55:05 +0300

    Moved turn code into player. Made proper get-active-race method.

--- game.rkt
+++ game.rkt
@@ -8,7 +8,11 @@
 
 ;; Races
 
-(struct race (race-banner special-power coins in-decline) #:transparent)
+(struct race (race-banner 
+              special-power 
+              [coins #:mutable]
+              [in-decline #:mutable])
+  #:transparent)
 
 (define (new-race race-banner special-power)
   (race race-banner special-power 0 #f))
@@ -17,12 +21,12 @@
 ;; Game
 (define game% 
   (class object% 
-    (init players)
-    
     (super-new)
     
-    (define current-players players)
-    (define/public (get-players) current-players) 
+    (init-field players)
+    (define/public (get-players) players) 
+    (for ([p players])
+      (set-field! game p this))
     
     (define world (new-world two-player-map))
     (define/public (get-world) world)
@@ -49,22 +53,16 @@
     
     (define/public (get-races) races)
     
-    (define/public (play-turn)
-      (for ([p current-players])
-        (let* ([race-index ((strategy-pick-a-race (player-strategy p)) this p)]
-               [race (list-ref races race-index)])
-          (send p add-race! race)
-          (set! races (remove-nth races race-index))
-          (add-new-race!))
-        
-        (map (lambda (r)
-               (let ([race (get-active-race p)]
-                     [tokens-to-conquer (+ 2 (length (get-tokens world r)))])
-                 (set-tokens! world r (make-list tokens-to-conquer (race-race-banner race))))) 
-             ((strategy-conquer (player-strategy p)) this p)))
-      
-      (set! turn (add1 turn)))
+    (define/public (take-race index)
+      (let ([race (list-ref races index)])
+        (set! races (remove-nth races index))
+        (add-new-race!)
+        race))
     
+    (define/public (play-turn)
+      (for ([p players])
+        (send p play-turn))
+      (set! turn (add1 turn)))
     ))
 
 (define all-race-banners 
@@ -87,11 +85,28 @@
     
     (init-field name)
     (init-field strategy)
+    (field [game #f])
     (field [points 5])
     (field [races '()])
     
+    (define/public (get-active-race)
+      (findf (lambda (r) (not (race-in-decline r))) 
+             races))
+    
     (define/public (add-race! race)
       (set! races (cons race races)))
+    
+    (define/public (play-turn)
+      (let* ([race-index ((strategy-pick-a-race strategy) this)])
+        (add-race! (send game take-race race-index)))
+      
+      (map (lambda (r)
+             (let ([race (get-active-race)]
+                   [tokens-to-conquer (+ 2 (length (get-tokens (send game get-world) r)))])
+               (set-tokens! (send game get-world) 
+                            r 
+                            (make-list tokens-to-conquer (race-race-banner race)))))
+           ((strategy-conquer strategy) this)))
     
     ))
 
@@ -103,25 +118,22 @@
 (define player-points (class-field-accessor player% points))
 (define player-races (class-field-accessor player% races))
 
-(define (get-active-race player) 
-  (first (player-races player)))
-
 
 ;; Strategy
 
 (struct strategy (pick-a-race ready-troops conquer redeploy go-into-decline))
 
-(define (new-strategy #:pick-a-race [pick-a-race (lambda (game player) 0)]
-                      #:ready-troops [ready-troops (lambda (game player) '())]
-                      #:conquer [conquer (lambda (game player) '())]
-                      #:redeploy [redeploy (lambda (game player) #f)]
-                      #:go-into-decline [go-into-decline (lambda (game player) #f)])
+(define (new-strategy #:pick-a-race [pick-a-race (lambda (player) 0)]
+                      #:ready-troops [ready-troops (lambda (player) '())]
+                      #:conquer [conquer (lambda (player) '())]
+                      #:redeploy [redeploy (lambda (player) #f)]
+                      #:go-into-decline [go-into-decline (lambda (player) #f)])
   (strategy pick-a-race ready-troops conquer redeploy go-into-decline))
 
 
 ;; Tests
 
-(define p1 (new-player "Vasya" (new-strategy #:conquer (lambda (game player) '(2)))))
+(define p1 (new-player "Vasya" (new-strategy #:conquer (lambda (player) '(2)))))
 (check-equal? (player-name p1) "Vasya")
 
 (define p2 (new-player "Petya" (new-strategy)))
@@ -155,3 +167,13 @@
 (check-equal? (get-tokens w 2) '(amazons amazons))
 (check-equal? (send g get-turn) 2)
 (check-equal? (length (send g get-races)) 6)
+
+
+; Player test
+(define p (new-player "Vasya" #f))
+(check-false (send p get-active-race))
+(define r (new-race 'amazons 'berserk))
+(send p add-race! r)
+(check-equal? (send p get-active-race) r)
+(set-race-in-decline! r #t)
+(check-false (send p get-active-race))
